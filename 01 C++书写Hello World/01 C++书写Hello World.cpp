44eeac20d408c721e70ccfbf823725ed1909aca6
Irray <1719076141@qq.com>
2021-12-04 18:19:57 +0800

    添加项目文件。

---
+++ 01 C++书写Hello World/01 C++书写Hello World.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+#include<vector>
+#include<algorithm>
+#include<numeric>
+
+
+int main()
+{
+     const pair<int, string> valueSymbols[] =
+     {
+          {1000,"M"},
+          {900,"CM"},
+          {500,"D"},
+          {400,"CD"},
+          {100,"C"}
+     };
+	system("pause");
+	return 0;
+}
